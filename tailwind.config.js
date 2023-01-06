f1610465924faaa041bbecce29968b212264e91f
Muhammad Khalid <talhah.jan@gmail.com>
2023-01-07 03:00:43 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js,php}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  darkMode: "class", // or media & false
+  theme: {
+    extend: {
+      padding: {
+        "1/3": "33.333333%",
+        "2/3": "66.666667",
+      },
+      fontFamily: {
+        Courgette: ["Courgette, cursive"],
+      },
+    },
+  },
+  plugins: [
+    require("tw-elements/dist/plugin"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
